9d4921da71a8b0ac808be89624a1a4ecdd4a8da5
Dimitris Couchell <dimitris@squareup.com>
2013-07-16 19:33:35 -0700

    WIP

---
+++ picasso/src/test/java/com/squareup/picasso/ImageViewRequestTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2013 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.squareup.picasso;
+
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.widget.ImageView;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static com.squareup.picasso.Request.LoadedFrom.MEMORY;
+import static com.squareup.picasso.TestUtils.BITMAP_1;
+import static com.squareup.picasso.TestUtils.RESOURCE_ID_1;
+import static com.squareup.picasso.TestUtils.URI_1;
+import static com.squareup.picasso.TestUtils.URI_KEY_1;
+import static com.squareup.picasso.TestUtils.mockImageViewTarget;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class ImageViewRequestTest {
+
+  @Test(expected = AssertionError.class)
+  public void throwsErrorWithNullResult() throws Exception {
+    ImageViewRequest request =
+        new ImageViewRequest(mock(Picasso.class), URI_1, 0, mockImageViewTarget(), null, null,
+            false, false, 0, null, URI_KEY_1, null);
+    request.complete(null, MEMORY);
+  }
+
+  @Test @Ignore // Context mocking
+  public void invokesSuccessIfTargetIsNotNull() throws Exception {
+    ImageView target = mockImageViewTarget();
+    Picasso mock = mock(Picasso.class);
+    ImageViewRequest request =
+        new ImageViewRequest(mock, mock(Uri.class), 0, target, null, null, false,
+            false, 0, null, "", null);
+    request.complete(BITMAP_1, MEMORY);
+    verify(target).setImageDrawable(any(PicassoDrawable.class));
+  }
+
+  @Test
+    public void invokesErrorIfTargetIsNotNullWithErrorResourceId() throws Exception {
+    ImageView target = mockImageViewTarget();
+    Picasso mock = mock(Picasso.class);
+    ImageViewRequest request =
+        new ImageViewRequest(mock, mock(Uri.class), 0, target, null, null, false,
+            false, RESOURCE_ID_1, null, "", null);
+    request.error();
+    verify(target).setImageResource(RESOURCE_ID_1);
+  }
+
+  @Test
+  public void invokesErrorIfTargetIsNotNullWithErrorDrawable() throws Exception {
+    Drawable errorDrawable = mock(Drawable.class);
+    ImageView target = mockImageViewTarget();
+    Picasso mock = mock(Picasso.class);
+    ImageViewRequest request =
+        new ImageViewRequest(mock, mock(Uri.class), 0, target, null, null, false,
+            false, 0, errorDrawable, "", null);
+    request.error();
+    verify(target).setImageDrawable(errorDrawable);
+  }
+}
